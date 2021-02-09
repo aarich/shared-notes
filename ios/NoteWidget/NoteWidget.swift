688c80087ce1c7f49acb00fbdaf7d8fc0435bea2
Alex Rich <arich@hmc.edu>
2021-02-09 11:19:43 -0800

    colors and stuff

--- ios/NoteWidget/NoteWidget.swift
+++ ios/NoteWidget/NoteWidget.swift
@@ -33,7 +33,7 @@ struct Provider: IntentTimelineProvider {
     
     if (slug == nil) {
       print("xxx slug was nil")
-      completion(makeEntry(title: "Your Note Here!", content: "Open the App to Create a Note"))
+      completion(makeEntry(title: "Your Note Here!", content: "Tap here to create a note, then press and hold here and select \"Edit Widget\""))
       return
     }
     
@@ -158,23 +158,22 @@ func makeEntry(name: String, content: String, lastModified: Date) -> SimpleEntry
 
 extension UIColor {
     public convenience init?(hex: String) {
-        let r, g, b, a: CGFloat
+        let r, g, b: CGFloat
 
         if hex.hasPrefix("#") {
             let start = hex.index(hex.startIndex, offsetBy: 1)
             let hexColor = String(hex[start...])
 
-            if hexColor.count == 8 {
+            if hexColor.count == 6 {
                 let scanner = Scanner(string: hexColor)
                 var hexNumber: UInt64 = 0
 
                 if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+                    g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+                    b = CGFloat((hexNumber & 0x0000ff)) / 255
 
-                    self.init(red: r, green: g, blue: b, alpha: a)
+                    self.init(red: r, green: g, blue: b, alpha: 1)
                     return
                 }
             }
