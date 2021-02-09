866a580bfe4259885d479e0f1fe11ff564f14622
Alex Rich <a.rich@salesforce.com>
2021-02-09 11:19:43 -0800

    colors and stuff

--- ios/NoteWidget/NoteWidgetView.swift
+++ ios/NoteWidget/NoteWidgetView.swift
@@ -9,26 +9,38 @@ import Foundation
 import SwiftUI
 
 struct NoteWidgetEntryView : View {
+  
+  static let formatter = RelativeDateTimeFormatter()
+
   var entry: Provider.Entry
   
   var body: some View {
     ZStack{
       Color(entry.color)
       VStack(alignment: .leading) {
+        Spacer()
+
         if (entry.showTitle) {
           Text(entry.name)
             .font(.system(size:13))
             .bold()
         }
         if (entry.showModified) {
-          Text(entry.lastModified, style: .relative)
-            .font(.system(size: 12))
+          Text("Modified \(entry.lastModified, formatter: Self.formatter)")
+            .font(.system(size: 10))
+        }
+        if (entry.showTitle || entry.showModified) {
+          Spacer()
         }
         
         Text(entry.content)
-          .font(.system(size:12))        
+          .font(.system(size:12))
+        
+        Spacer()
+        Spacer()
+
       }
-      .padding(.all)
+      .padding(15)
     }
     
   }
